584a83b0f1ebed586c08154c1c4b3ec58fa7d164
Ryan Cross <rcross@amsl.com>
2013-04-25 21:29:11 +0000

    add fix2.py  - Legacy-Id: 70

---
+++ mlarchive/bin/fix2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+'''
+Fix unindented Received lines.
+
+Based on a short survey we are assuming Received lines immediately follow
+envelope.  We use the "From:" header to signify done reading Received lines.
+
+
+to run first do
+export DJANGO_SETTINGS_MODULE=mlarchive.settings
+'''
+
+import sys
+sys.path.insert(0, '/a/home/rcross/src/amsl/mailarch/trunk')
+#sys.path.insert(0, '/a/home/rcross/src/amsl/mlabast/mlarchive')
+
+from django.core.management import setup_environ
+from django.db.utils import IntegrityError
+from mlarchive import settings
+
+setup_environ(settings)
+
+from mlarchive.archive.models import *
+from email.utils import parseaddr
+import glob
+import mailbox
+import re
+
+def main():
+    PATTERN = re.compile(r'^[a-zA-Z\-]: ')
+    with open('/a/home/rcross/tmp/fix2.txt') as f:
+        files = f.read().splitlines()
+        
+    #files = ('/a/www/ietf-mail-archive/text/policy/1999-04.mail',)
+    for file in files:
+        mb = mailbox.mbox(file)
+        for m in mb:
+            count = 0
+            for line in splitlines(m.as_string()):
+                count += 1
+                if line.startswith('Received: ') or line.startswith(' '):
+                    continue
+                if PATTERN.match(line):
+                    break
+                print "%s:%s:%s" % (file,count,line)
+        mb.close()
+        
+if __name__ == "__main__":
+    main()

b2698d74dfcec01096e75e3495e400b989e45928
Ryan Cross <rcross@amsl.com>
2013-10-12 23:22:53 +0000

    changes  - Legacy-Id: 187

--- mlarchive/archive/generator.py
+++ mlarchive/archive/generator.py
@@ -34,6 +34,19 @@ def skip_attachment(function):
         return function(*args, **kwargs)
     return _inner
 
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 # --------------------------------------------------
 # Classes
 # --------------------------------------------------
--- mlarchive/archive/models.py
+++ mlarchive/archive/models.py
@@ -12,6 +12,7 @@ from email.Header import decode_header
 from bs4 import BeautifulSoup
 from HTMLParser import HTMLParser, HTMLParseError
 #from html2text import html2text
+from mlarchive.archive.generator import Generator
 
 import mailbox
 import os
@@ -30,194 +31,7 @@ logger = getLogger('mlarchive.custom')
 # --------------------------------------------------
 # Helper Functions
 # --------------------------------------------------
-def get_charset(part):
-    '''
-    Get the character set from the Content-Type.
-    Use DEFAULT CHARSET if it isn't set.
-    '''
-    charset = part.get_content_charset()
-    return charset if charset else DEFAULT_CHARSET
 
-def skip_attachment(function):
-    '''
-    This is a decorator for custom MIME part handlers, handle_*.
-    If the part passed is an attachment then it is skipped (None is returned).
-    '''
-    def _inner(*args, **kwargs):
-        if args[0].get_filename():
-            return None
-        return function(*args, **kwargs)
-    return _inner
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
-
-def handle_external_body(part,text_only):
-    '''
-    Two common formats:
-    A) in content type parameters
-    Content-Type: Message/External-body; name="draft-ietf-alto-reqs-03.txt";
-        site="ftp.ietf.org"; access-type="anon-ftp";
-        directory="internet-drafts"
-
-    Content-Type: text/plain
-    Content-ID: <2010-02-17021922.I-D@ietf.org>
-
-    B) as an attachment
-    Content-Type: message/external-body; name="draft-howlett-radsec-knp-01.url"
-    Content-Description: draft-howlett-radsec-knp-01.url
-    Content-Disposition: attachment; filename="draft-howlett-radsec-knp-01.url";
-        size=92; creation-date="Mon, 14 Mar 2011 22:39:25 GMT";
-        modification-date="Mon, 14 Mar 2011 22:39:25 GMT"
-    Content-Transfer-Encoding: base64
-
-    W0ludGVybmV0U2hvcnRjdXRdDQpVUkw9ZnRwOi8vZnRwLmlldGYub3JnL2ludGVybmV0LWRyYWZ0
-    cy9kcmFmdC1ob3dsZXR0LXJhZHNlYy1rbnAtMDEudHh0DQo=
-    '''
-    if text_only:
-        return None
-
-    # handle B format
-    if part.get_filename() and part.get_filename().endswith('url'):
-        codec = part['Content-Transfer-Encoding']
-        inner = part.get_payload()
-        payload = inner[0].get_payload()
-        link = payload.decode(codec)
-        return link
-    # handle A format
-    else:
-        rawsite = part.get_param('site')
-        site = collapse_rfc2231_value(rawsite)
-        rawdir = part.get_param('directory')
-        dir = collapse_rfc2231_value(rawdir)
-        rawname = part.get_param('name')
-        name = collapse_rfc2231_value(rawname)
-        link = 'ftp://%s/%s/%s' % (site,dir,name)
-        html = '<div><a rel="nofollow" href="%s">&lt;%s&gt;</a></div>' % (link,link)
-        return html
-
-@skip_attachment
-def handle_html(part,text_only):
-    '''
-    Handler for text/HTML MIME parts.  Takes a message.message part and a boolean, text_only,
-    which specifies if the handler should return text only, for use with indexing
-    '''
-    if not text_only:
-        payload = part.get_payload(decode=True)
-        charset = part.get_content_charset()
-        uni = unicode(payload,charset or DEFAULT_CHARSET,errors='replace')
-        return render_to_string('archive/message_html.html', {'payload': uni})
-    else:
-        payload = part.get_payload(decode=True)
-        uni = unicode(payload,errors='ignore')
-        # tried many solutions here
-        # text = strip_tags(part.get_payload(decode=True)) # problems with bad html
-        # soup = BeautifulSoup(part.get_payload(decode=True)) # errors with lxml
-        # text = html2text(uni) # errors with malformed tags
-        soup = BeautifulSoup(part.get_payload(decode=True),'html5') # included "html" and css
-        text = soup.get_text()
-
-        return text
-
-@skip_attachment
-def handle_plain(part,text_only):
-    charset = get_charset(part)
-    payload = part.get_payload(decode=True)
-    if charset not in US_CHARSETS and charset not in UNSUPPORTED_CHARSETS:
-        try:
-            payload = payload.decode(charset)
-        except UnicodeDecodeError as error:
-            logger.warn("UnicodeDecodeError [{0}, {1}]".format(error.encoding,error.reason))
-            payload = unicode(payload,DEFAULT_CHARSET,errors='ignore')
-        except LookupError as error:
-            logger.warn("Decode Error [{0}, {1}]".format(error.args,error.message))
-            payload = unicode(payload,DEFAULT_CHARSET,errors='ignore')
-
-    result = render_to_string('archive/message_plain.html', {'payload': payload})
-    # undeclared charactersets can cause problems with template rendering
-    # if result is empty template (len=28) try again with unicode
-    if len(result) == 28 and not isinstance(payload, unicode):
-        payload = unicode(payload,DEFAULT_CHARSET,errors='ignore')
-        result = render_to_string('archive/message_plain.html', {'payload': payload})
-    return result
-
-# a dictionary of supported mime types
-HANDLERS = {'message/external-body':handle_external_body,
-            'text/plain':handle_plain,
-            'text/html':handle_html}
-
-def handle(part,text_only):
-    type = part.get_content_type()
-    #logger.debug('handling %s' % type)
-    handler = HANDLERS.get(type,None)
-    if handler:
-        return handler(part,text_only)
-
-def parse_entity(entity, text_only=False):
-    '''
-    This function recursively traverses a MIME email and returns a list of email.Message objects
-    '''
-    #print "calling parse %s:%s" % (entity.__class__,entity.get_content_type())
-    parts = []
-    # messages with type message/external-body are marked multipart, but we need to treat them
-    # otherwise
-    if entity.is_multipart() and entity.get_content_type() != 'message/external-body':
-        if entity.get_content_type() == 'multipart/alternative':
-            contents = entity.get_payload()
-            # NOTE: rather than trying to handle possibly malformed HTML, just use the
-            # text/plain versions for display.
-            # --clip
-            # if output is not for indexing start from the most detailed option
-            # if not text_only:
-            #     contents = contents[::-1]
-            # --clip
-            for x in contents:
-                # only return first readable item
-                r = parse_entity(x,text_only)
-                if r:
-                    parts.extend(r)
-                    break
-        else:
-            for part in entity.get_payload():
-                parts.extend(parse_entity(part,text_only))
-    else:
-        body = handle(entity,text_only)
-        if body:
-            parts.append(body)
-
-    #print "returning parse %s:%s" % (type(parts),parts)
-    return parts
-
-def parse_body(msg, text_only=False, request=None):
-    try:
-        with open(msg.get_file_path()) as f:
-            mm = mailbox.MaildirMessage(f)
-            headers = mm.items()
-            parts = parse_entity(mm,text_only)
-    except IOError:
-        return 'Error reading message'
-
-    if not text_only:
-        return render_to_string('archive/message.html', {
-            'msg': msg,
-            'maildirmessage': mm,
-            'headers': headers,
-            'parts': parts,
-            'request': request}
-        )
-    else:
-        return '\n'.join(parts)
 
 # --------------------------------------------------
 # Models
@@ -268,7 +82,7 @@ class Message(models.Model):
             mhout = subprocess.check_output(TXT2HTML,stdin=f)
         soup = BeautifulSoup(mhout)
         return soup.body
-        
+
     def get_absolute_url(self):
         return '/archive/detail/%s/%s' % (self.email_list.name,self.hashcode)
 
@@ -283,10 +97,12 @@ class Message(models.Model):
         Returns the contents of the message body, text only for use in indexing.
         ie. HTML is stripped.
         '''
-        return parse_body(self, text_only=True)
+        gen = Generator(self)
+        return gen.as_text()
 
     def get_body_html(self, request=None):
-        return parse_body(self, request=request)
+        gen = Generator(self)
+        return gen.as_html(request=request)
 
     def get_body_raw(self):
         '''

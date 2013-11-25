ecd6ea76470563023dc5b5b70ca65609066e2f9d
Ryan Cross <rcross@amsl.com>
2013-11-25 01:57:59 +0000

    add haystack 2.1  - Legacy-Id: 239

---
+++ haystack/urls.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
+
+from haystack.views import SearchView
+
+
+urlpatterns = patterns('haystack.views',
+    url(r'^$', SearchView(), name='haystack_search'),
+)

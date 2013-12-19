b6c11afcce8d54f15e6a5140166a6f88aa33631c
Ryan Cross <rcross@amsl.com>
2013-12-19 04:01:06 +0000

    make call-archives compatible with old mail-archive arguments  - Legacy-Id: 270

--- mlarchive/bin/call-archives.py
+++ mlarchive/bin/call-archives.py
@@ -6,6 +6,10 @@ The script should be configured in mm_cfg.py as follows:
 PUBLIC_EXTERNAL_ARCHIVER = '/a/ietf/scripts/call-archives.py %(listname)s --public'
 PRIVATE_EXTERNAL_ARCHIVER = '/a/ietf/scripts/call-archives.py %(listname)s --private'
 
+This script is also called from /a/postfix/aliases.  Example:
+
+ietfarch-itu+ietf-web-archive:          "|/a/ietf/scripts/call_archives.py itu+ietf -web"
+
 The script will save STDIN and use it to make calls to both archive systems.  It first calls the
 mhonarc system.  If that fails we return exit code of the mhonarc script, allowing for retries
 from mailman.  If it succeeds we proceed to the mail archive.
@@ -20,16 +24,24 @@ import sys
 MHONARC = '/a/ietf/scripts/archive-mail.pl'
 MAILARCH= '/a/mailarch/current/mlarchive/bin/archive-mail.py'
 
-args = sys.argv[1:]
+old_args = sys.argv[1:]
+new_args = old_args[:]
+
+if len(old_args) > 1:
+    if old_args[1].startswith('--'):                    # convert to single dash
+        old_args[1] = old_args[1][1:]
+    if new_args[1] not in ('--public','--private'):     # dump unsupported options
+        new_args.pop(1)
+
 data = sys.stdin.read()
 
-p = subprocess.Popen([MHONARC, args[0], args[1][1:]], stdin=subprocess.PIPE)    # strip dash
+p = subprocess.Popen([MHONARC] + old_args, stdin=subprocess.PIPE)
 p.communicate(input=data)
 if p.returncode != 0:
     sys.exit(p.returncode)
 
 if os.path.exists('/a/mailarch/data/mailman.enable'):
-    p = subprocess.Popen([MAILARCH] + args, stdin=subprocess.PIPE)
+    p = subprocess.Popen([MAILARCH] + new_args, stdin=subprocess.PIPE)
     p.communicate(input=data)
 
 sys.exit(0)

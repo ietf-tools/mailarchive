36e7989a5a9e04da622b510642e10b69ed4e67b3
Ryan Cross <rcross@amsl.com>
2014-04-28 22:45:57 +0000

    add staging settings file  - Legacy-Id: 336

---
+++ mlarchive/settings/staging.py
@@ -0,0 +1,30 @@
+# settings/development.py
+from .base import *
+
+# NOTE: DEBUG must be false for long running message imports
+DEBUG=False
+
+DATA_ROOT = '/a/mailarch/data_test'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'test-cache'
+    }
+}
+
+# HAYSTACK SETTINGS
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.BaseSignalProcessor'
+#HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
+HAYSTACK_XAPIAN_PATH = os.path.join(DATA_ROOT,'archive_index')
+HAYSTACK_CONNECTIONS['default']['PATH'] = HAYSTACK_XAPIAN_PATH
+
+# ARCHIVE SETTINGS
+ARCHIVE_DIR = os.path.join(DATA_ROOT,'archive')
+CONSOLE_STATS_FILE = os.path.join(DATA_ROOT,'log','console.json')
+LOG_FILE = os.path.join(DATA_ROOT,'log','mlarchive.log')
+SERVER_MODE = 'development'
+
+LOGGING['handlers']['watched_file']['filename'] = LOG_FILE
+LOGGING['handlers']['archive-mail_file_handler']['filename'] = os.path.join(DATA_ROOT,'log','archive-mail.log')
+

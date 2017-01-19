c2379b8b7c5b22f99931f5d382f1a9a865e53651
Ryan Cross <rcross@amsl.com>
2017-01-19 21:01:43 +0000

    Add virtualenv support to archive-mail (main import) script.  - Legacy-Id: 559

--- mlarchive/bin/archive-mail.py
+++ mlarchive/bin/archive-mail.py
@@ -10,8 +10,15 @@ The default is public.
 import os
 import sys
 path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Virtualenv support
+virtualenv_activation = os.path.join(path, "bin", "activate_this.py")
+if os.path.exists(virtualenv_activation):
+    execfile(virtualenv_activation, dict(__file__=virtualenv_activation))
+
 if not path in sys.path:
     sys.path.insert(0, path)
+
 import django
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
     os.environ['DJANGO_SETTINGS_MODULE'] = 'mlarchive.settings.production'

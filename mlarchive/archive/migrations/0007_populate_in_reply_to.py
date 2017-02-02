0ce81437db4cdc8dfbb78161aa9122e949682ed0
Ryan Cross <rcross@amsl.com>
2017-02-02 17:30:09 +0000

    Resolve issue with archive 0007_populate_in_reply_to migration.  - Legacy-Id: 566

--- mlarchive/archive/migrations/0007_populate_in_reply_to.py
+++ mlarchive/archive/migrations/0007_populate_in_reply_to.py
@@ -1,23 +1,71 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import re
+
 from django.db import models, migrations
 
 from mlarchive.archive.models import Message, get_in_reply_to_message
 
+MESSAGE_ID_RE = re.compile(r'<(.*?)>')
+
+
+def batch_qs(qs, batch_size=1000):
+    """
+    Returns a (start, end, total, queryset) tuple for each batch in the given
+    queryset.
+    
+    Usage:
+        # Make sure to order your querset
+        article_qs = Article.objects.order_by('id')
+        for start, end, total, qs in batch_qs(article_qs):
+            print "Now processing %s - %s of %s" % (start + 1, end, total)
+            for article in qs:
+                print article.body
+    """
+    total = qs.count()
+    for start in range(0, total, batch_size):
+        end = min(start + batch_size, total)
+        yield (start, end, total, qs[start:end])
 
 def get_replies(apps, schema_editor):
     '''Traverse all message records, read in_reply_to_value and 
     populate in_reply_to FK'''
     Message = apps.get_model("archive", "Message")
 
-    for message in Message.objects.all():
-        reply_message = get_in_reply_to_message(
-            message.in_reply_to_value,
-            message.email_list)
-        if reply_message:        
-            message.in_reply_to = reply_message
-            message.save()
+    message_qs = Message.objects.order_by('id')
+    for start, end, total, qs in batch_qs(message_qs):
+        for message in qs:
+            msgids = MESSAGE_ID_RE.findall(message.in_reply_to_value)
+            if not msgids:
+                continue
+        
+            msgid = msgids[0]
+            try:
+                reply_message = Message.objects.get(msgid=msgid, email_list=message.email_list)
+            except Message.DoesNotExist:
+                reply_message = Message.objects.filter(msgid=msgid).first()
+        
+            if reply_message:
+                try:
+                    message.in_reply_to = reply_message
+                    message.save()
+                except ValueError:
+                    import sys
+                    print "message:{}:{}".format(message.pk,type(message))
+                    print "reply_message:{}:{}".format(reply_message.pk,type(reply_message))
+                    sys.exit()
+
+def get_in_reply_to_message(in_reply_to_value, email_list):
+    '''Returns the in_reply_to message, if it exists'''
+    
+    if not msgids:
+        return None
+    return get_message_prefer_list(msgids[0],email_list)
+
+def get_message_prefer_list(msgid, email_list):
+    '''Returns Message (or None) prefers proivded list'''
+    Message = apps.get_model("archive", "Message")
 
 
 def reverse_get_replies(apps, schema_editor):

c226b6a01e3fc12b951d13993a1a1ecae5e637eb
Ryan Cross <rcross@amsl.com>
2017-04-19 23:59:08 +0000

    Remove some commented code  - Legacy-Id: 599

--- mlarchive/archive/utils.py
+++ mlarchive/archive/utils.py
@@ -36,25 +36,3 @@ def jsonapi(fn):
         return HttpResponse(json.dumps(context_data),content_type='application/json')
     return to_json
 
-'''
-def render(template, data, request):
-    return render_to_response(template,
-                              data,
-                              context_instance=RequestContext(request))
-
-def template(template):
-    def decorator(fn):
-        def render(request, *args, **kwargs):
-            context_data = fn(request, *args, **kwargs)
-            if isinstance(context_data, HttpResponse):
-                # View returned an HttpResponse like a redirect
-                return context_data
-            else:
-                # For any other type of data try to populate a template
-                return render_to_response(template,
-                        context_data,
-                        context_instance=RequestContext(request)
-                    )
-        return render
-    return decorator
-'''

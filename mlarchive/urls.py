6859524a94b5cdfcc33b2ca07c304ba6876638a2
Ryan Cross <rcross@amsl.com>
2017-04-21 19:01:33 +0000

    Upgrade to Django 1.11.  Fixes #2269.  - Legacy-Id: 601

--- mlarchive/urls.py
+++ mlarchive/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import include, url
 from django.views.generic import RedirectView
 
 from django.contrib import admin
-from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(pattern_name='archive',permanent=True)),
-    url(r'^accounts/login/$', login, {'template_name': 'archive/login.html'}),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='archive/login.html')),
     url(r'^arch/', include('mlarchive.archive.urls')),
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),

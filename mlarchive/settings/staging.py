4fbc87f8a04f601edb8208fdb68885af063f663c
Ryan Cross <rcross@amsl.com>
2017-10-04 19:21:26 +0000

    Change staging directory  - Legacy-Id: 663

--- mlarchive/settings/staging.py
+++ mlarchive/settings/staging.py
@@ -5,7 +5,7 @@ from .base import *
 DEBUG=False
 
 DATA_ROOT = '/a/mailarch/data'
-TEST_ROOT = '/a/mailarch/data_test'
+STAGING_ROOT = '/a/mailarch/data_staging'
 
 CACHES = {
     'default': {
@@ -17,15 +17,15 @@ CACHES = {
 # HAYSTACK SETTINGS
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.BaseSignalProcessor'
 #HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
-HAYSTACK_XAPIAN_PATH = os.path.join(TEST_ROOT,'xapian.stub')
+HAYSTACK_XAPIAN_PATH = os.path.join(STAGING_ROOT,'xapian.stub')
 HAYSTACK_CONNECTIONS['default']['PATH'] = HAYSTACK_XAPIAN_PATH
 
 # ARCHIVE SETTINGS
 ARCHIVE_DIR = os.path.join(DATA_ROOT,'archive')
-CONSOLE_STATS_FILE = os.path.join(DATA_ROOT,'log','console.json')
-LOG_FILE = os.path.join(DATA_ROOT,'log','mlarchive.log')
+CONSOLE_STATS_FILE = os.path.join(STAGING_ROOT,'log','console.json')
+LOG_FILE = os.path.join(STAGING_ROOT,'log','mlarchive.log')
 SERVER_MODE = 'development'
 
 LOGGING['handlers']['watched_file']['filename'] = LOG_FILE
-LOGGING['handlers']['archive-mail_file_handler']['filename'] = os.path.join(DATA_ROOT,'log','archive-mail.log')
+LOGGING['handlers']['archive-mail_file_handler']['filename'] = os.path.join(STAGING_ROOT,'log','archive-mail.log')
 

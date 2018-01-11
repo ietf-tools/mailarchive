15eb76109802d5f95b50e8c3cf8c91c2259fb566
Ryan Cross <rcross@amsl.com>
2018-01-11 00:04:52 +0000

    Add some utility scripts  - Legacy-Id: 702

---
+++ mlarchive/bin/fix_attachments.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+'''
+This script will recreate attachments for specified messages
+'''
+
+# Standalone broilerplate -------------------------------------------------------------
+from django_setup import do_setup
+do_setup(settings='production')
+# -------------------------------------------------------------------------------------
+
+import email
+from mlarchive.archive.models import Message
+from mlarchive.archive.management.commands import _classes
+
+pks = [2108476,1210205,1210328,1300179,1440463,2108877,1517225,2108475]
+
+for pk in pks:
+    message = Message.objects.get(pk=pk)
+    with open(message.get_file_path()) as f:
+        msg = email.message_from_file(f)
+    mw = _classes.MessageWrapper(msg,message.email_list.name)
+    mw._archive_message = message
+    message.attachment_set.all().delete()
+    mw.process_attachments()
+    print "MSG {}: {}".format(pk, message.attachment_set.all())
+

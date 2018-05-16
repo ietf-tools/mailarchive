c63700c0c1eabe5afb17c4a1992e08770a7fbc77
Ryan Cross <rcross@amsl.com>
2018-05-16 17:05:38 +0000

    Add ordering and search to EmailList admin page  - Legacy-Id: 766

--- mlarchive/archive/admin.py
+++ mlarchive/archive/admin.py
@@ -6,8 +6,11 @@ from mlarchive.archive.models import Message, EmailList, Attachment, Thread
 class MessageAdmin(admin.ModelAdmin):
     raw_id_fields = ('email_list', 'in_reply_to', 'thread')
 
+class EmailListAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
 
 admin.site.register(Message, MessageAdmin)
-admin.site.register(EmailList)
+admin.site.register(EmailList, EmailListAdmin)
 admin.site.register(Attachment)
 admin.site.register(Thread)

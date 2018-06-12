f932c36831f7d0c156d8e4c061345dfa1fb7269d
Ryan Cross <rcross@amsl.com>
2018-06-12 23:12:37 +0000

    Flake8 changes  - Legacy-Id: 784

--- mlarchive/tests/archive/management/commands/_classes.py
+++ mlarchive/tests/archive/management/commands/_classes.py
@@ -2,6 +2,7 @@ import datetime
 import email
 import glob
 import mailbox
+import os
 import pytest
 import pytz
 import shutil
@@ -9,15 +10,18 @@ import shutil
 
 from django.conf import settings
 from django.urls import reverse
-from mlarchive.archive.management.commands._classes import *
+from mlarchive.archive.models import Message
+from mlarchive.archive.management.commands._classes import (archive_message, clean_spaces, MessageWrapper,
+    get_base_subject, get_envelope_date, tzoffset, get_from, get_header_date, get_mb, get_mime_extension,
+    is_aware, get_received_date, parsedate_to_datetime, subject_is_reply)
 from factories import EmailListFactory, MessageFactory, ThreadFactory
-from pprint import pprint
 
 
 def teardown_module(module):
-    for path in (settings.LOG_FILE,settings.IMPORT_LOG_FILE):
+    for path in (settings.LOG_FILE, settings.IMPORT_LOG_FILE):
         if os.path.exists(path):
             os.remove(path)
+
 
 @pytest.mark.django_db(transaction=True)
 def test_archive_message(client):
@@ -32,7 +36,7 @@ Hello,
 
 This is a test email.  database
 '''
-    status = archive_message(data,'test',private=False)
+    status = archive_message(data, 'test', private=False)
     assert status == 0
     # ensure message in db
     assert Message.objects.all().count() == 1
@@ -46,6 +50,7 @@ This is a test email.  database
     url = reverse('archive_search') + '/?q=tdate:20131107175455'
     assert len(response.context['results']) == 1
 
+
 @pytest.mark.django_db(transaction=True)
 def test_archive_message_fail(client):
     data = '''Hello,
@@ -54,34 +59,36 @@ This is a test email.  With no headers
 '''
     # remove any existing failed messages
     publist = EmailListFactory.create(name='public')
-    shutil.rmtree(publist.failed_dir,ignore_errors=True)
+    shutil.rmtree(publist.failed_dir, ignore_errors=True)
 
-    status = archive_message(data,'public',private=False)
+    status = archive_message(data, 'public', private=False)
     assert status == 1
     assert Message.objects.all().count() == 0
-    filename = os.path.join(publist.failed_dir,
-            datetime.datetime.today().strftime('%Y-%m-%d') + '.0000')
+    filename = os.path.join(publist.failed_dir, datetime.datetime.today().strftime('%Y-%m-%d') + '.0000')
     assert os.path.exists(filename)
     os.remove(filename)                         # cleanup
+
 
 def test_archive_message_bad_order():
     # test that index thread id / date correct if older message added later
     # TODO
     pass
 
+
 def test_clean_spaces():
     s = 'this     is   a    string   with extra    spaces'
     assert clean_spaces(s) == 'this is a string with extra spaces'
 
+
 def test_get_base_subject():
     data = [('[ANCP] =?iso-8859-1?q?R=FCckruf=3A_Welcome_to_the_=22ANCP=22_mail?=\n\t=?iso-8859-1?q?ing_list?=',
              u'R\xfcckruf: Welcome to the "ANCP" mailing list'),
-            ('Re: [IAB] Presentation at IAB Tech Chat (fwd)',u'Presentation at IAB Tech Chat'),
-            ('Re: [82companions] [Bulk]  Afternoon North Coast tour',u'Afternoon North Coast tour'),
-            ('''[cdni-footprint] Fwd: Re: [CDNi] Rough Agenda for today's "CDNI Footprint/Capabilties Design Team Call"''',u'''Rough Agenda for today's "CDNI Footprint/Capabilties Design Team Call"'''),
-            ('[dccp] [Fwd: Re: [Tsvwg] Fwd: WGLC for draft-ietf-dccp-quickstart-03.txt]',u'WGLC for draft-ietf-dccp-quickstart-03.txt'),
-            ('[ANCP] Fw: note the boilerplate change',u'note the boilerplate change'),
-            ('Re: [ANCP] RE: draft-ieft-ancp-framework-00.txt',u'draft-ieft-ancp-framework-00.txt')]
+            ('Re: [IAB] Presentation at IAB Tech Chat (fwd)', u'Presentation at IAB Tech Chat'),
+            ('Re: [82companions] [Bulk]  Afternoon North Coast tour', u'Afternoon North Coast tour'),
+            ('''[cdni-footprint] Fwd: Re: [CDNi] Rough Agenda for today's "CDNI Footprint/Capabilties Design Team Call"''', u'''Rough Agenda for today's "CDNI Footprint/Capabilties Design Team Call"'''),
+            ('[dccp] [Fwd: Re: [Tsvwg] Fwd: WGLC for draft-ietf-dccp-quickstart-03.txt]', u'WGLC for draft-ietf-dccp-quickstart-03.txt'),
+            ('[ANCP] Fw: note the boilerplate change', u'note the boilerplate change'),
+            ('Re: [ANCP] RE: draft-ieft-ancp-framework-00.txt', u'draft-ieft-ancp-framework-00.txt')]
 
     message = email.message_from_string('From: rcross@amsl.com')
     mw = MessageWrapper(message, 'test')
@@ -90,11 +97,12 @@ def test_get_base_subject():
         base = get_base_subject(normal)
         assert base == item[1]
 
+
 def test_get_envelope_date():
     data = [('From iesg-bounces@ietf.org Fri Dec 01 02:58:00 2006',
-            datetime.datetime(2006,12,1,2,58)),                          # normal
+            datetime.datetime(2006, 12, 1, 2, 58)),                          # normal
             ('From denis.pinkas at bull.net  Fri Feb  1 03:12:00 2008',
-            datetime.datetime(2008,2,1,3,12)),                           # obfiscated
+            datetime.datetime(2008, 2, 1, 3, 12)),                           # obfiscated
             ('From fluffy@cisco.com Thu, 15 Jul 2004 17:15:16 -0700 (PDT)',
             datetime.datetime(2004, 7, 15, 17, 15, 16, tzinfo=tzoffset(None, -25200))),   # double tzinfo
             ('From Kim.Fullbrook@O2.COM Tue, 01 Feb 2005 06:01:13 -0500',
@@ -104,10 +112,11 @@ def test_get_envelope_date():
             ('From scott.mcglashan@hp.com Wed, 6 Jul 2005 12:24:15 +0100 (BST)',
             datetime.datetime(2005, 7, 6, 12, 24, 15, tzinfo=tzoffset(None, 3600))),      # odd tzinfo
             ('From scoya  Fri Sep  1 02:28:00 2000',
-            datetime.datetime(2000,9,1,2,28))]                           # simple from
+            datetime.datetime(2000, 9, 1, 2, 28))]                           # simple from
     for item in data:
         message = email.message_from_string(item[0])
         assert get_envelope_date(message) == item[1]
+
 
 def test_get_from():
     data = '''From rcross@amsl.com Fri Feb 21 11:09:00 2014
@@ -122,23 +131,24 @@ Hello.
     assert get_from(message) == 'From rcross@amsl.com Fri Feb 21 11:09:00 2014'
 
     # test mailbox.mboxMessage
-    path = os.path.join(settings.BASE_DIR,'tests','data','mbox.1')
+    path = os.path.join(settings.BASE_DIR, 'tests', 'data', 'mbox.1')
     mb = mailbox.mbox(path)
     assert get_from(mb[0]) == 'internet-drafts@ietf.org  Wed Aug 21 16:20:36 2013'
 
+
 def test_get_header_date():
     data = [('Date: Wed, 08 Apr 92 13:31:28 -0400',
-            datetime.datetime(1992,4,8,17,31,28)),      # 2-digit year, numeric tz
+            datetime.datetime(1992, 4, 8, 17, 31, 28)),      # 2-digit year, numeric tz
             ('Date: Thu, 28 May 92 9:18:58 PDT',
-            datetime.datetime(1992,5,28,16,18,58)),     # 2-digit year, char tz
+            datetime.datetime(1992, 5, 28, 16, 18, 58)),     # 2-digit year, char tz
             ('Date: Thursday, 28 May 1992 12:06:31 EDT',
-            datetime.datetime(1992,5,28,16,06,31)),     # full day name, char tz
+            datetime.datetime(1992, 5, 28, 16, 06, 31)),     # full day name, char tz
             ('Date: Fri, 29 May 1992 18:02:45 -0400 (EDT)',
-            datetime.datetime(1992,5,29,22,02,45)),     # numeric & char tz
+            datetime.datetime(1992, 5, 29, 22, 02, 45)),     # numeric & char tz
             ('Date: 27 Jan 2002 21:46:45 +0000',
-            datetime.datetime(2002,1,27,21,46,45)),     # no day of week
+            datetime.datetime(2002, 1, 27, 21, 46, 45)),     # no day of week
             ('Sent: Thu, 28 May 92 9:18:58 PDT',
-            datetime.datetime(1992,5,28,16,18,58))]     # Sent header
+            datetime.datetime(1992, 5, 28, 16, 18, 58))]     # Sent header
     for item in data:
         message = email.message_from_string(item[0])
         date = get_header_date(message)
@@ -146,17 +156,20 @@ def test_get_header_date():
         date = date.astimezone(pytz.utc).replace(tzinfo=None)
         assert date == item[1]
 
+
 def test_get_mb():
-    files = glob.glob(os.path.join(settings.BASE_DIR,'tests','data','mailbox_*'))
+    files = glob.glob(os.path.join(settings.BASE_DIR, 'tests', 'data', 'mailbox_*'))
     for file in files:
         mb = get_mb(file)
         assert len(mb) > 0
 
+
 def test_get_mime_extension():
-    data = [('image/jpeg','jpg'),('text/html','html'),('hologram/3d','bin')]
+    data = [('image/jpeg', 'jpg'), ('text/html', 'html'), ('hologram/3d', 'bin')]
     for item in data:
         ext, desc = get_mime_extension(item[0])
         assert ext == item[1]
+
 
 def test_get_received_date():
     data = '''Received: from mail.ietf.org ([64.170.98.30]) by localhost \
@@ -166,38 +179,42 @@ for <ancp@ietfa.amsl.com>; Tue, 29 Jan 2013 00:08:57 -0800 (PST)'''
     date = get_received_date(message)
     # convert to naive if we get a timezone aware object
     date = date.astimezone(pytz.utc).replace(tzinfo=None)
-    assert date == datetime.datetime(2013,1,29,8,8,57)
+    assert date == datetime.datetime(2013, 1, 29, 8, 8, 57)
+
 
 def test_is_aware():
-    assert is_aware(datetime.datetime(2013,1,1)) == False
-    assert is_aware(datetime.datetime(2013,1,1,12,0,0,0,pytz.UTC)) == True
+    assert is_aware(datetime.datetime(2013, 1, 1)) is False
+    assert is_aware(datetime.datetime(2013, 1, 1, 12, 0, 0, 0, pytz.UTC)) is True
+
 
 def test_parsedate_to_datetime():
     date = 'Tue Jan  1 2014 12:30:00 PST'
     result = parsedate_to_datetime(date)
-    assert isinstance(result,datetime.datetime)
+    assert isinstance(result, datetime.datetime)
     # TODO: assert we have the correct timezone
 
+
 def test_subject_is_reply():
-    assert subject_is_reply('This is a test') == False
-    assert subject_is_reply('Re: This is a test') == True
-    assert subject_is_reply('RE: This is a test') == True
-    assert subject_is_reply('Re: [list] This is a test') == True
-    assert subject_is_reply('Fwd: This is a test')  == True
-    assert subject_is_reply('[Fwd: This is a test]') == True
-    assert subject_is_reply('This is a test (fwd)') == True
-    assert subject_is_reply('[list] Fwd: Re: This is a test') == True
-    assert subject_is_reply('[public] [Fwd: Re: [list] Fwd: This is a test]') == True
+    assert subject_is_reply('This is a test') is False
+    assert subject_is_reply('Re: This is a test') is True
+    assert subject_is_reply('RE: This is a test') is True
+    assert subject_is_reply('Re: [list] This is a test') is True
+    assert subject_is_reply('Fwd: This is a test') is True
+    assert subject_is_reply('[Fwd: This is a test]') is True
+    assert subject_is_reply('This is a test (fwd)') is True
+    assert subject_is_reply('[list] Fwd: Re: This is a test') is True
+    assert subject_is_reply('[public] [Fwd: Re: [list] Fwd: This is a test]') is True
 
-#def test_save_failed_msg
+# def test_save_failed_msg
 
-#def test_BetterMbox
+# def test_BetterMbox
 
-#def test_Loader()
+# def test_Loader()
 
 # --------------------------------------------------
 # MessageWrapper
 # --------------------------------------------------
+
 
 def test_MessageWrapper_get_addresses():
     data = [('ancp@ietf.org',                               # simple
@@ -209,6 +226,7 @@ def test_MessageWrapper_get_addresses():
     for item in data:
         assert MessageWrapper.get_addresses(item[0]) == item[1]
 
+
 def test_MessageWrapper_get_cc():
     data = '''From: joe@acme.com
 To: larry@acme.com
@@ -219,8 +237,9 @@ Date: Mon, 24 Feb 2014 08:04:41 -0800
 This is the message.
 '''
     msg = email.message_from_string(data)
-    mw = MessageWrapper(msg,'ancp')
+    mw = MessageWrapper(msg, 'ancp')
     assert mw.get_to() == 'larry@acme.com'
+
 
 @pytest.mark.django_db(transaction=True)
 def test_MessageWrapper_get_thread():
@@ -231,12 +250,12 @@ def test_MessageWrapper_get_thread():
     list2 = EmailListFactory.create(name='list2')
     thread1 = ThreadFactory.create()
     thread2 = ThreadFactory.create()
-    message1 = MessageFactory.create(
+    MessageFactory.create(
         email_list=list1,
         msgid='001@example.com',
         thread=thread1,
         date=datetime.datetime(2016, 1, 1))
-    message2 = MessageFactory.create(
+    MessageFactory.create(
         email_list=list2,
         msgid='001@example.com',
         thread=thread2,
@@ -252,15 +271,16 @@ Date: Mon, 24 Feb 2016 08:04:41 -0800
 This is the message.
 '''
     msg = email.message_from_string(data)
-    mw = MessageWrapper(msg,'list1')
+    mw = MessageWrapper(msg, 'list1')
     assert mw.archive_message.thread == thread1
+
 
 @pytest.mark.django_db(transaction=True)
 def test_MessageWrapper_get_thread_subject():
     '''Test subject based threading'''
     elist = EmailListFactory.create(name='public')
     thread = ThreadFactory.create()
-    message = MessageFactory.create(
+    MessageFactory.create(
         email_list=elist,
         msgid='001@example.com',
         subject='[public] New Members',
@@ -276,8 +296,9 @@ Date: Mon, 24 Feb 2016 08:04:41 -0800
 This is the message.
 '''
     msg = email.message_from_string(data)
-    mw = MessageWrapper(msg,'public')
+    mw = MessageWrapper(msg, 'public')
     assert mw.archive_message.thread == thread
+
 
 @pytest.mark.django_db(transaction=True)
 def test_MessageWrapper_get_thread_from_header():
@@ -292,9 +313,10 @@ Date: Mon, 24 Feb 2014 08:04:41 -0800
 This is the message.
 '''.format(msgid=message.msgid)
     msg = email.message_from_string(data)
-    mw = MessageWrapper(msg,'public')
+    mw = MessageWrapper(msg, 'public')
     mw.process()
     assert mw.get_thread_from_header(mw.references) == message.thread
+
 
 def test_MessageWrapper_get_to():
     data = '''From: joe@acme.com
@@ -306,9 +328,8 @@ Date: Mon, 24 Feb 2014 08:04:41 -0800
 This is the message.
 '''
     msg = email.message_from_string(data)
-    mw = MessageWrapper(msg,'ancp')
+    mw = MessageWrapper(msg, 'ancp')
     assert mw.get_cc() == 'ancp@ietf.org'
 
 # test various exceptions raised
-
 # test that older message added causes update to all tdates in index

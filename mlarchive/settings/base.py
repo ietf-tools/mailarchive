0f33f53241738fe5caccba3bdf49516f71a25ea5
Ryan Cross <rcross@amsl.com>
2018-09-21 05:23:50 +0000

    More bootstrap4 migration work  - Legacy-Id: 856

--- mlarchive/settings/base.py
+++ mlarchive/settings/base.py
@@ -133,7 +133,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.admindocs',
     'django.contrib.humanize',
-    'bootstrap3',
+    'bootstrap4',
     'celery_haystack',
     'haystack',
     'htauth',

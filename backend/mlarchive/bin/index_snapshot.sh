53be9fe94a14dc426b1cb3b1e79e48c1412a98d2
Ryan Cross <rcross@amsl.com>
2020-02-27 18:45:45 +0000

    Have bin/index_snapshot.sh output nothing when successful  - Legacy-Id: 964

--- backend/mlarchive/bin/index_snapshot.sh
+++ backend/mlarchive/bin/index_snapshot.sh
@@ -3,5 +3,9 @@
 # Create a snapshot of the mail archive elasticsearch index
 
 # PUT /_snapshot/my_backup/<snapshot-{now/d}>
-curl -s -X PUT "localhost:9200/_snapshot/es_snapshot_repo/%3Csnapshot-%7Bnow%2Fd%7D%3E"
+OUT=`curl -s -X PUT "localhost:9200/_snapshot/es_snapshot_repo/%3Csnapshot-%7Bnow%2Fd%7D%3E"`
+
+if [ "$OUT" != '{"accepted":true}' ]; then
+    echo "$OUT"
+fi
 

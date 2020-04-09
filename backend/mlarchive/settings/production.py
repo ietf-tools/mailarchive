02a81d9e44c2a4e216eaa4fb4e845ecba641f49c
Ryan Cross <rcross@amsl.com>
2020-04-09 17:41:19 +0000

    Update settings and INSTALL instructions  - Legacy-Id: 996

--- backend/mlarchive/settings/production.py
+++ backend/mlarchive/settings/production.py
@@ -1,5 +1,22 @@
 # settings/production.py
 from .base import *
 
+# -------------------------------------
+# DJANGO SETTINGS
+# -------------------------------------
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['.ietf.org', '.amsl.com']
+ADMINS = (('Ryan Cross', 'rcross@amsl.com'))
+
+
+# -------------------------------------
+# CUSTOM SETTINGS
+# -------------------------------------
+
 # CLOUDFLARE  INTEGRATION
 USING_CDN = True
+CLOUDFLARE_AUTH_EMAIL = get_secret("CLOUDFLARE_AUTH_EMAIL")
+CLOUDFLARE_AUTH_KEY = get_secret("CLOUDFLARE_AUTH_KEY")
+CLOUDFLARE_ZONE_ID = get_secret("CLOUDFLARE_ZONE_ID")

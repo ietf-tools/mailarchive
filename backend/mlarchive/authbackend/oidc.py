5934b79be8f9161f6fa701a26b9f4764771410dd
Ryan Cross <rcross@amsl.com>
2021-02-04 17:08:40 +0000

    Add OIDC backend files  - Legacy-Id: 1071

---
+++ backend/mlarchive/authbackend/oidc.py
@@ -0,0 +1,7 @@
+import unicodedata
+
+def generate_username(email):
+    # Using Python 3 and Django 1.11+, usernames can contain alphanumeric
+    # (ascii and unicode), _, @, +, . and - characters. So we normalize
+    # it and slice at 150 characters.
+    return unicodedata.normalize('NFKC', email)[:150]

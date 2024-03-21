ed78add92f5dbf1fdee99052b79b45e76ad2ba80
Ryan Cross <rcross@amsl.com>
2024-03-21 10:38:50 +1000

    fix: fix collect static

--- build/app/settings_collectstatics.py
+++ build/app/settings_collectstatics.py
@@ -1,5 +1,5 @@
 from .base import *
-from mailarchive import __version__
+from mlarchive import __version__
 
 STATIC_URL = "https://static.ietf.org/ma/%s/"%__version__
 STATIC_ROOT = os.path.abspath(BASE_DIR + "/../../static/")

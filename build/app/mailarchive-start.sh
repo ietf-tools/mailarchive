46e315189edffb1cc76be1590148084d63dcaffa
Ryan Cross <rcross@amsl.com>
2024-03-21 14:20:39 +1000

    fix: use gunicorn instead of runserver (#3720)

--- build/app/mailarchive-start.sh
+++ build/app/mailarchive-start.sh
@@ -10,4 +10,10 @@ echo "Running Initializing index..."
 ./backend/manage.py init_index
 
 echo "Starting Mailarchive..."
-./backend/manage.py runserver 0.0.0.0:8000
+gunicorn \
+          --workers 10 \
+          --max-requests 32768 \
+          --timeout 180 \
+          --log-level info \
+          --bind :8000 \
+          backend.mlarchive.wsgi:application

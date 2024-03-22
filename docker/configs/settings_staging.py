503666c4e365f0c755c0af5b66331a75779d591b
Ryan Cross <rcross@amsl.com>
2024-03-22 12:30:11 +1000

    fix: fix settings_staging

--- docker/configs/settings_staging.py
+++ docker/configs/settings_staging.py
@@ -1,6 +1,9 @@
 # settings/settings_staging.py
 from .base import *
 
+from mlarchive import __version__
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = "https://static.ietf.org/mailarchive/%s/"%__version__
 STATIC_ROOT = os.path.abspath(BASE_DIR + "/../../static/")
 

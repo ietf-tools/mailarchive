7473df9556e1c55b5c26b678ac360da18b80bea4
Ryan Cross <rcross@amsl.com>
2024-05-02 16:43:56 -0700

    fix: create pv for dev (#3756)

--- docker/configs/settings_staging.py
+++ docker/configs/settings_staging.py
@@ -1,11 +1,12 @@
 # settings/settings_staging.py
 from .base import *
 
-from mlarchive import __version__
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# from mlarchive import __version__
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = "https://static.ietf.org/mailarchive/%s/"%__version__
-STATIC_ROOT = os.path.abspath(BASE_DIR + "/../../static/")
+# STATIC_URL = "https://static.ietf.org/mailarchive/%s/"%__version__
+# STATIC_ROOT = os.path.abspath(BASE_DIR + "/../../static/")
+
 
 AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend"]
 ELASTICSEARCH_SIGNAL_PROCESSOR = 'mlarchive.archive.signals.CelerySignalProcessor'

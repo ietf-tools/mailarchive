b51fdbbbf70339987925ae264e2eb64133fb5cf4
Ryan Cross <rcross@amsl.com>
2024-06-13 14:15:04 -0700

    fix: clean up docker files

--- docker/configs/settings_docker.py
+++
@@ -1,6 +0,0 @@
-# settings/settings_rebuild.py
-from .base import *
-
-AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend"]
-# ELASTICSEARCH_SIGNAL_PROCESSOR = 'mlarchive.archive.signals.RealtimeSignalProcessor'
-ELASTICSEARCH_SIGNAL_PROCESSOR = 'mlarchive.archive.signals.CelerySignalProcessor'

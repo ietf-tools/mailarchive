ad81ea25bfa600913259171c45b39854e813caa8
Ryan Cross <rcross@amsl.com>
2024-06-19 09:54:29 -0700

    fix: add gunicorn signal trap (#3794)

--- build/app/mailarchive-start.sh
+++ build/app/mailarchive-start.sh
@@ -10,10 +10,28 @@ echo "Running Initializing index..."
 ./backend/manage.py init_index
 
 echo "Starting Mailarchive..."
+
+# trap TERM and shut down gunicorn
+cleanup () {
+    if [[ -n "${gunicorn_pid}" ]]; then
+        echo "Terminating gunicorn..."
+        kill -TERM "${gunicorn_pid}"
+        wait "${gunicorn_pid}"
+    fi
+}
+
+trap 'trap "" TERM; cleanup' TERM
+
+# start gunicorn in the background so we can trap the TERM signal
 gunicorn \
-          --workers 10 \
-          --max-requests 32768 \
-          --timeout 180 \
-          --log-level info \
+          --workers "${GUNICORN_WORKERS:-9}" \
+          --max-requests "${GUNICORN_MAX_REQUESTS:-32768}" \
+          --timeout "${GUNICORN_TIMEOUT:-180}" \
+          --log-level "${GUNICORN_LOG_LEVEL:-info}" \
           --bind :8000 \
-          backend.mlarchive.wsgi:application
+          --capture-output \
+          --access-logfile -\
+          ${GUNICORN_EXTRA_ARGS} \
+          backend.mlarchive.wsgi:application &
+gunicorn_pid=$!
+wait "${gunicorn_pid}"

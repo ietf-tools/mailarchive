aeb1b5970df52a41b1204a4a84c44cc1cbe1bbf0
Ryan Cross <rcross@amsl.com>
2024-07-22 15:46:57 -0700

    fix: fix celery import issue (#3810)

--- backend/mlarchive/celeryapp.py
+++ backend/mlarchive/celeryapp.py
@@ -1,18 +1,19 @@
 import os
-import celery
+from celery import Celery
+from celery.signals import setup_logging
 
 from django.conf import settings
 
 
 # Disable celery's internal logging configuration, we set it up via Django
-@celery.signals.setup_logging.connect
+@setup_logging.connect
 def on_setup_logging(**kwargs):
     pass
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mlarchive.settings.settings')
 
-app = celery.Celery('mlarchive')
+app = Celery('mlarchive')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 

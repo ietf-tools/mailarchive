f476e0ad1d743d7a3d7da77cdee70d18070e5ffe
Ryan Cross <rcross@amsl.com>
2024-08-15 16:42:22 -0700

    chore: add sandbox deployment scripts (#3817)
    * chore: add sandbox deployment scripts
    
    * fix: tweak cli
    
    * fix: use versioned image

---
+++ dev/deploy-to-container/settings_sandbox.py
@@ -0,0 +1,21 @@
+
+import os
+from .base import *
+
+DATABASES = {
+    'default': {
+        'HOST': '__DBHOST__',
+        'PORT': 5432,
+        'NAME': 'mailarch',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'mailarch',
+        'PASSWORD': 'franticmarble',
+    },
+}
+
+SECRET_KEY = "__SECRETKEY__"
+
+CELERY_BROKER_URL = '__MQCONNSTR__'
+
+# OIDC configuration
+SITE_URL = 'https://__HOSTNAME__'

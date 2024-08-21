fad6f4a2f6ec73e492fe47749a064d408b57f4f7
Ryan Cross <rcross@amsl.com>
2024-08-20 17:32:03 -0700

    ci: fix load_messages (#3829)

--- dev/deploy-to-container/load_messages.sh
+++ dev/deploy-to-container/load_messages.sh
@@ -14,10 +14,10 @@ curdle/***
 EOL
 
 # sync
-rsync -av --include-from="$INCLUDE_FILE" --exclude='*' rsync.ietf.org::mailman-archive/ ./
+rsync -a --include-from="$INCLUDE_FILE" --exclude='*' rsync.ietf.org::mailman-archive/ ./
 
 # load data
 cd /workspace/backend
-./manage.py load /tmp/yang-doctors/2018-10.mail
-./manage.py load /tmp/curdle/2017-05.mail
-./manage.py load /tmp/mtgvenue/2016-07.mail
+./manage.py load /tmp/yang-doctors/2018-10.mail -l yang-doctors
+./manage.py load /tmp/curdle/2017-05.mail -l curdle
+./manage.py load /tmp/mtgvenue/2016-07.mail -l mtgvenue

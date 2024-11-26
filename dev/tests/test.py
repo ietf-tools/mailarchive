3616a6ac17d2274943d9fcece777edc1fb355c8f
Ryan Cross <rcross@amsl.com>
2024-11-26 15:48:48 -0800

    ci: run tests on pull_request

---
+++ dev/tests/test.py
@@ -0,0 +1,52 @@
+# settings/test.py
+import os
+from .base import *
+
+DATA_ROOT = '/data'
+
+DATABASES = {
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+
+# ELASTICSEARCH SETTINGS
+ELASTICSEARCH_INDEX_NAME = 'test-mail-archive'
+ELASTICSEARCH_SILENTLY_FAIL = True
+ES_URL = 'http://{}:9200/'.format(env('ELASTICSEARCH_HOST'))
+ELASTICSEARCH_CONNECTION = {
+    'URL': ES_URL,
+    'INDEX_NAME': 'test-mail-archive',
+    'http_auth': ('elastic', 'changeme'),
+}
+ELASTICSEARCH_SIGNAL_PROCESSOR = 'mlarchive.archive.signals.RealtimeSignalProcessor'
+
+# use standard default of 20 as it's easier to test
+ELASTICSEARCH_RESULTS_PER_PAGE = 20
+SEARCH_RESULTS_PER_PAGE = 20
+SEARCH_SCROLL_BUFFER_SIZE = SEARCH_RESULTS_PER_PAGE
+
+# ARCHIVE SETTINGS
+ARCHIVE_DIR = os.path.join(DATA_ROOT, 'archive')
+STATIC_INDEX_DIR = os.path.join(DATA_ROOT, 'static')
+LOG_FILE = os.path.join(BASE_DIR, 'tests/tmp', 'mlarchive.log')
+
+SERVER_MODE = 'development'
+
+LOGGING['handlers']['mlarchive']['filename'] = LOG_FILE
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+# IMAP Interface
+EXPORT_DIR = os.path.join(DATA_ROOT, 'export')
+
+
+# CLOUDFLARE  INTEGRATION
+USING_CDN = False
